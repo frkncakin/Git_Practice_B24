ef7973162b87ef60d6905f4a2217e94e85244b8d
frkncakin <frkncakin@gmail.com>
2021-10-03 15:37:56 -0400

    made car in master branch

---
+++ src/setup/Car.java
@@ -0,0 +1,13 @@
+package setup;
+
+public class Car {
+
+    // this is car class from master branch
+
+    // we will have confluct
+
+    // because there is different text in different branches for car
+
+
+
+}
